750e27b956d2192ec6c88e8fbf42feb6c87cc787
Aaron C <git@acwpd.com>
2017-06-11 17:19:22 -0700

    gitignore and private_settings

---
+++ src/example_private_settings.php
@@ -0,0 +1,11 @@
+<?php
+
+	if (ENVIRONMENT === 'Development') {
+		DEFINE('MYSQL_Server','localhost');
+	} else {
+		DEFINE('MYSQL_Server','something.else.com');
+	}
+	DEFINE('MYSQL_Read_User','read-only');		
+	DEFINE('MYSQL_Read_Password','Read-Password');	// Stored plaintext. This file is in your gitignore, right?
+	DEFINE('MYSQL_Write_User','Read-Write');		
+	DEFINE('MYSQL_Write_Password','RW-Password');	// Stored plaintext. This file is in your gitignore, right?
